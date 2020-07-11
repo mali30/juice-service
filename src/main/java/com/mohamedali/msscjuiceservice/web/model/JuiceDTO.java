b595751fd0aa0546019d19e592ef77673a6df52f
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-07-11 15:03:49 -0400

    updated juice object with validation annotations

--- src/main/java/com/mohamedali/msscjuiceservice/web/model/JuiceDTO.java
+++ src/main/java/com/mohamedali/msscjuiceservice/web/model/JuiceDTO.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -15,13 +19,32 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class JuiceDTO {
+    @Null
     private UUID uuid;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private String juiceName;
+
+    @NotNull
     private JuiceStyleEnum juiceStyle;
+
+    @NotNull
+    @Positive
     private Long upc;
+
     private Integer quantityOnHand;
+
+    @Positive
+    @NotNull
+    private Integer price;
 
 }
