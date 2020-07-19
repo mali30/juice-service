152a80022f9c0e05027c6327b1ae22d4ed816d92
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-07-19 13:21:26 -0400

    added juice repo

--- src/main/java/com/mohamedali/msscjuiceservice/repositories/JuiceRepository.java
+++ src/main/java/com/mohamedali/msscjuiceservice/repositories/JuiceRepository.java
@@ -1,11 +1,11 @@
 package com.mohamedali.msscjuiceservice.repositories;
 
-import com.mohamedali.msscjuiceservice.domain.Juice;
+import com.mohamedali.msscjuiceservice.domain.JuiceEntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface JuiceRepository extends PagingAndSortingRepository<Juice , UUID> {
+public interface JuiceRepository extends PagingAndSortingRepository<JuiceEntity, UUID> {
 
 
 
