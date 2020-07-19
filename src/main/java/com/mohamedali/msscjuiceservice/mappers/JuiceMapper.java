875d15552294741cea2f35e5d7d5714a07598ac6
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-07-19 13:20:48 -0400

    added juice mapper

--- src/main/java/com/mohamedali/msscjuiceservice/mappers/JuiceMapper.java
+++ src/main/java/com/mohamedali/msscjuiceservice/mappers/JuiceMapper.java
@@ -1,4 +1,12 @@
 package com.mohamedali.msscjuiceservice.mappers;
 
+import com.mohamedali.msscjuiceservice.domain.JuiceEntity;
+import com.mohamedali.msscjuiceservice.web.model.JuiceDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
 public interface JuiceMapper {
+
+    JuiceEntity juiceEntityToJuiceDTO(JuiceDTO juiceDTO);
+    JuiceDTO juiceDTOToJuiceEntity(JuiceEntity juiceEntity);
 }
